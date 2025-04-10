20269c9381001aa4be9d123f8ee84ff564cac8e1
unknown <lauriegale10@gmail.com>
2025-04-10 17:29:57 +0100

    Add session data for exercise logs (#11)

--- clean_logs.py
+++ clean_logs.py
@@ -1,7 +1,12 @@
 from classes.ExerciseLog import ExerciseLog
 from classes.StageLog import StageLog
 from classes.WindowFocusEvent import WindowFocusEvent
+from classes.TimestampParser import TimestampParser
 from enums import DebuggingStage, FocusType
+from loading_services.fetch_logs_from_file import fetch_data_from_json
+from save_logs import *
+
+import datetime
 
 def format_start_end_times(exercise_log: ExerciseLog) -> ExerciseLog:
     """Reformats the stage and exercise logs such that they contain start and end times to them, making it easier to work out temporal-related data.
@@ -38,3 +43,21 @@ def clean_focus_events(stage_log: StageLog) -> StageLog:
             i += 1
         stage_log.focus_events = cleaned_focus_events
     return stage_log
+
+def add_session_data():
+    raw_student_ids: list[dict] = fetch_data_from_json("data/student_ids")
+    student_id_strings: list[str] = [student_id['id'] for student_id in raw_student_ids]
+    raw_exercise_logs: list[dict] = fetch_data_from_json("data/exercise_logs")
+    for student_id in student_id_strings:
+        student_exercise_logs: list[dict] = [exercise_log for exercise_log in raw_exercise_logs if exercise_log['studentId'] == student_id]
+        #Create sorted list of dates
+        if len(student_exercise_logs) > 0:
+            exercise_dates_per_student: list[str] = sorted(set(TimestampParser.parse_timestamp_str(exercise_log["time"]).strftime("%Y-%m-%d") for exercise_log in student_exercise_logs))
+        #Add session number to each exercise log
+        for log in student_exercise_logs:
+            parsed_log_timestamp: str = TimestampParser.parse_timestamp_str(log["time"]).strftime("%Y-%m-%d")
+            log["session"] = exercise_dates_per_student.index(parsed_log_timestamp) + 1
+    #Save exercise logs to file
+    save_exercise_logs(raw_exercise_logs, "exercise_logs_with_session_data")
+
+add_session_data()
