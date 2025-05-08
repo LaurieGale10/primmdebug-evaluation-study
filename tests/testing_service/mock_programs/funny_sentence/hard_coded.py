46836fb6e979d23b71ba74673f482e1a202c1f23
unknown <lauriegale10@gmail.com>
2025-05-08 15:18:33 +0100

    Update funny_sentence tests

---
+++ tests/testing_service/mock_programs/funny_sentence/hard_coded.py
@@ -0,0 +1,4 @@
+number = int(input("Enter a number: "))
+adjective = input("Enter an adjective: ")
+animal = input("Enter an animal: ")
+print(f"Wow! I just saw 64 sneaky elephants juggling pineapples down the street!")
--- tests/testing_service/program_tests/test_funny_sentence_test.py
+++ tests/testing_service/program_tests/test_funny_sentence_test.py
@@ -8,10 +8,16 @@ class TestFunnySentenceTest(unittest.TestCase):
 
     def test_orginal_program(self):
         self.assertEqual(FunnySentenceTest.run_tests("tests/testing_service/mock_programs/funny_sentence/original"), (0, 3))
+
+    def test_empty_program(self):
+        self.assertEqual(FunnySentenceTest.run_tests("tests/testing_service/mock_programs/funny_sentence/empty"), (0, 3))
     
     def test_correct_with_concatenation_program(self):
         self.assertEqual(FunnySentenceTest.run_tests("tests/testing_service/mock_programs/funny_sentence/correct_with_concatenation"), (3, 3))
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_incorrect_output_program(self):
+        self.assertEqual(FunnySentenceTest.run_tests("tests/testing_service/mock_programs/funny_sentence/incorrect_output"), (1, 3))
+    
+    def test_hard_coded_program(self):
+        self.assertEqual(FunnySentenceTest.run_tests("tests/testing_service/mock_programs/funny_sentence/hard_coded"), (2, 3))
+    
