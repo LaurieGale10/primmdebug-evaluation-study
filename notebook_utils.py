bf861b7663f810b669f696fa7223924d392bd8d2
unknown <lauriegale10@gmail.com>
2025-05-24 12:01:27 +0100

    Add further AHC implementation and plotting of silhouette widths

--- notebook_utils.py
+++ notebook_utils.py
@@ -1,3 +1,6 @@
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
 
 def display_percentage_string(x: int | float, y: int | float, decimal_places: int = 2) -> str:
     """Display a percentage string in the form 'x/y (percentage%)'"""
@@ -5,3 +8,32 @@ def display_percentage_string(x: int | float, y: int | float, decimal_places: in
         return f"{x}/{y} (N/A)"
     percentage: float = (x / y) * 100
     return f"{x}/{y} ({percentage:.{decimal_places}f}%)"
+
+def plot_silhouette_plot(cluster_labels, silhouette_values, silhouette_score, n_clusters):
+    fig, ax = plt.subplots()
+
+    y_lower = 10
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = silhouette_values[cluster_labels == i]
+
+        ith_cluster_silhouette_values.sort()
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax.fill_betweenx(
+            np.arange(y_lower, y_upper),
+            0,
+            ith_cluster_silhouette_values,
+            facecolor=color,
+            edgecolor=color,
+            alpha=0.7,
+        )
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10  # 10 for the 0 samples
+    ax.axvline(x=silhouette_score, color="red", linestyle="--")
+    return fig, ax
