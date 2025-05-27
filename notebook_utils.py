d06cfc931296e5e2f88e2ae7c24ca8523f0e64ee
unknown <lauriegale10@gmail.com>
2025-05-27 17:33:17 +0100

    Add more in-depth evaluation of clusters (#14)

--- notebook_utils.py
+++ notebook_utils.py
@@ -1,6 +1,19 @@
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+from pandas import DataFrame
+
+#Necessary rpy2 imports
+from rpy2.robjects.packages import importr, data
+from sklearn.metrics import pairwise_distances
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri, numpy2ri, conversion, default_converter
+from rpy2.robjects.vectors import StrVector
+
+utils = importr('utils')
+utils.install_packages(StrVector('WeightedCluster'))
+
+
 
 def display_percentage_string(x: int | float, y: int | float, decimal_places: int = 2) -> str:
     """Display a percentage string in the form 'x/y (percentage%)'"""
@@ -37,3 +50,23 @@ def plot_silhouette_plot(cluster_labels, silhouette_values, silhouette_score, n_
         y_lower = y_upper + 10  # 10 for the 0 samples
     ax.axvline(x=silhouette_score, color="red", linestyle="--")
     return fig, ax
+
+def calculate_cluster_evaluation_metrics(df: DataFrame, cluster_labels: np.ndarray | list[int], distance_metric = "euclidean") -> DataFrame:
+    weightedcluster_library = importr('WeightedCluster')
+    dissimilarity_matrix = pairwise_distances(df, metric="euclidean")
+    with (ro.default_converter + numpy2ri.converter).context():
+        # Convert to R matrix
+        diss_r = conversion.get_conversion().py2rpy(dissimilarity_matrix)
+        cluster_labels_r = conversion.get_conversion().py2rpy(cluster_labels + 1)  # R uses 1-based indexing
+        cluster_quality_result = weightedcluster_library.wcClusterQuality(
+            diss_r,
+            cluster_labels_r
+        )
+    point_biserial_correlation = cluster_quality_result[0][0]
+    average_silhouette_width = cluster_quality_result[0][4]
+    huberts_c = cluster_quality_result[0][9]
+    return {
+        "point_biserial_correlation": point_biserial_correlation,
+        "average_silhouette_width": average_silhouette_width,
+        "huberts_c": huberts_c
+    }
